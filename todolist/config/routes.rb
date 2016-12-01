2ff3c42f35e2eecfb73abc74f746f33023d99895
Rodrigo <rodrigotos3@hotmail.com>
2016-11-30 21:09:33 -0500

    Routes para devise

--- todolist/config/routes.rb
+++ todolist/config/routes.rb
@@ -1,5 +1,20 @@
 Rails.application.routes.draw do
+
+
+  devise_for :users, controllers: {
+          sessions: 'users/sessions',
+          passwords: 'users/passwords',
+          registrations: 'users/registrations'
+  }
+
+  get 'tasks/index'
+
+  get 'tasks/new'
+
+  get 'tasks/update'
+
+  get 'tasks/delete'
+
   root 'static_pages#home'
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
