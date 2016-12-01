4180ac84a69e6a4cd032eb644d6aab3449da2b3e
CARLOS <carloszambrano@me.com>
2016-11-30 21:54:30 -0500

    creacionde notebooks

--- todolist/config/routes.rb
+++ todolist/config/routes.rb
@@ -24,6 +24,7 @@ Rails.application.routes.draw do
 
   get 'tasks/delete'
 
+  resources :notebooks
 
   root 'static_pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
