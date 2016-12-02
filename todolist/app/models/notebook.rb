2d7806d295133855710a28818b37a1308e53e3aa
CARLOS <carloszambrano@me.com>
2016-12-01 21:48:53 -0500

    crud notebooks

--- todolist/app/models/notebook.rb
+++ todolist/app/models/notebook.rb
@@ -1,4 +1,4 @@
 class Notebook < ApplicationRecord
   has_many :tasks, class_name: "NotebookTask"
-  belongs_to :user, class_name: "User", foreign_key: "task_id"
+  belongs_to :user, class_name: "User", foreign_key: "id"
 end
